b3a419391bf3b57233f5f4902f8d1c966d581d42
jullian-yapeter <jullianyapeter@gmail.com>
2023-03-27 18:52:03 -0700

    Prototyped first variation of JudgeNet

--- judgenet/modules/models.py
+++ judgenet/modules/models.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch
 import torch.nn as nn
 from torch.distributions.independent import Independent
@@ -88,3 +90,74 @@ class TanhMLENet(MLENet):
         mu = x[:, 0].view(-1, 1)
         log_sigma = x[:, 1].view(-1, 1)
         return TanhNormal(mu, log_sigma.exp(), min=0.0, max=1.0)
+
+
+class JudgeNetSharedDecoder(nn.Module):
+
+    def __init__(self,
+                 in_names: List[str],
+                 in_dims: List[int],
+                 out_dim: int,
+                 emb_dim: int,
+                 hidden_dim: int,
+                 n_hidden_layers: int,
+                 dropout_rate: float,
+                 device=None):
+        assert len(in_names) == len(in_dims), "Length of names does not match length of in dims"
+        super().__init__()
+        self.device = torch.device('cpu') if device is None else device
+        self.in_names = in_names
+        self.unimodal_encoders = nn.ModuleDict()
+        self.unimodal_indices = {}
+        cur_index = 0
+        for in_name, in_dim in zip(in_names, in_dims):
+            self.unimodal_indices[in_name] = (cur_index, cur_index + in_dim)
+            cur_index += in_dim
+            self.unimodal_encoders[in_name] = LinearNet(
+                in_dim=in_dim,
+                out_dim=emb_dim,
+                hidden_dim=hidden_dim,
+                n_hidden_layers=n_hidden_layers,
+                dropout_rate=dropout_rate
+            )
+        self.multimodal_encoder = LinearNet(
+            in_dim=sum(in_dims),
+            out_dim=emb_dim,
+            hidden_dim=hidden_dim,
+            n_hidden_layers=n_hidden_layers,
+            dropout_rate=dropout_rate
+        )
+        self.predictor = LinearNet(
+            in_dim=emb_dim,
+            out_dim=out_dim,
+            hidden_dim=hidden_dim,
+            n_hidden_layers=n_hidden_layers,
+            dropout_rate=dropout_rate
+        )
+        self.ce_loss = nn.CrossEntropyLoss(reduction="mean")
+        self.mse_loss = nn.MSELoss(reduction='mean')
+
+    def forward(self, x):
+        x = x.to(self.device)
+        outputs = {}
+        for name in self.in_names:
+            start_idx, end_idx = self.unimodal_indices[name]
+            unimodal_input = x[:, start_idx: end_idx]
+            outputs[name] = self.unimodal_encoders[name](unimodal_input)
+        multimodal_emb = self.multimodal_encoder(x)
+        outputs["multimodal"] = multimodal_emb
+        outputs["prediction"] = self.predictor(multimodal_emb)
+        return outputs
+
+    def loss(self, outputs, label, alpha=0.5):
+        prediction_loss = self.ce_loss(
+            outputs["prediction"].to(self.device), label.to(self.device)
+        )
+        teacher_student_loss = 0
+        multimodal_emb = outputs["multimodal"].detach()
+        for name in self.in_names:
+            teacher_student_loss += self.mse_loss(
+                outputs[name].to(self.device), multimodal_emb
+            )
+        loss = alpha * prediction_loss + (1 - alpha) * teacher_student_loss
+        return loss
