b9f19296247c07be22f5a30641232a4a0f3eb9ea
jullian-yapeter <jullianyapeter@gmail.com>
2023-04-23 14:59:33 -0700

    updated results aggregator

--- judgenet/values/constants.py
+++ judgenet/values/constants.py
@@ -2,4 +2,4 @@
 multimodal_ae_filename = "multimodal_ae.pt"
 distill_net_filename = "distill_net.pt"
 final_model_filename = "final.pt"
-test_metrics_filename = "metrics.json"
+test_metrics_filename = "results.json"
