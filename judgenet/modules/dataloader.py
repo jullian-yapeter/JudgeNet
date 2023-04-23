aa7b1d54234d1e0ce75e0829be04a9142ea96ccd
jasonfisrael <jaisrael1@gmail.com>
2023-04-23 13:32:33 -0700

    Ted dataloader cache + dynamic threshold

--- judgenet/modules/dataloader.py
+++ judgenet/modules/dataloader.py
@@ -31,23 +31,35 @@ class ReindexDataset(Dataset):
         return self.data[self.idxs[i]][:-1], self.data[self.idxs[i]][-1]
     
 class TedDataset(Dataset):
-    def __init__(self, negative_ratio_threshold=.2):
+    def __init__(self):
         self.dataset = pd.read_csv("data/ted/dataset.csv")
+        score_ratios = self.dataset["neg_count"] / self.dataset["pos_count"]
+        negative_ratio_threshold = np.median(score_ratios)
         self.negative_ratio_threshold = negative_ratio_threshold
+
+        self.cache = {}
 
     def __len__(self):
         return len(self.dataset)
     
     def __getitem__(self, index):
+        # First check cache for this index
+        if index in self.cache:
+            return self.cache[index]
+        
         row = self.dataset.iloc[index]
         lexical_features = torch.load(f"data/{row['lexical_feature_path']}")
         audio_features = torch.load(f"data/{row['audio_feature_path']}")
+        features = torch.cat((lexical_features, audio_features),dim=-1).to(torch.float)
         negative_proportion = row["neg_count"] / row["pos_count"]
         if negative_proportion < self.negative_ratio_threshold:
             label = 0
         else:
             label = 1
-        return torch.cat((lexical_features, audio_features),dim=-1).to(torch.float), label
+
+        # Add row to cache
+        self.cache[index] = (features, label)
+        return features, label
         
 
 class MITInterviewDataset(Dataset):
@@ -92,24 +104,12 @@ class IEMOCAPDataset(object):
 
             # Perform mean pooling on visual features
             visual_features_pooled = np.mean(visual_features, axis=0)
-            
-            # np.zeros(visual_features.shape[1], dtype="float32")
-            # visual_frame_count = visual_features.shape[0]
-            # for i in range(visual_frame_count):
-            #     frame = visual_features[i]
-            #     visual_features_pooled += frame
-            # visual_features_pooled /= visual_frame_count
+
     
             acoustic_features = np.load("data/iemocap/" + row["acoustic_features"][1:])
 
             # Perform mean pooling on acoustic features
             acoustic_features_pooled = np.mean(acoustic_features, axis=0)
-            # acoustic_features_pooled = np.zeros(acoustic_features.shape[1], dtype="float32")
-            # acoustic_frame_count = acoustic_features.shape[0]
-            # for i in range(acoustic_frame_count):
-            #     frame = acoustic_features[i]
-            #     acoustic_features_pooled += frame
-            # acoustic_features_pooled /= acoustic_frame_count
 
             lexical_features = np.load("data/iemocap/" + row["lexical_features"][1:])
             label = row["emotion_labels"]
